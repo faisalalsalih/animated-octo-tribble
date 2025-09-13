4a73f43ea852a57556fe8de3b9ac1c9a6abb4b1f
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-09-13 22:13:38 +0500

    Last commit of 13/9/2025

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 const HomeHeroText = () => {
   return (
     <>
-    <div>
+    <div className='font-[font1] text-[#FFFFFF]'>
+    <div className='text-[9.5vw] leading-[10vw]'>
       THE SPARKS FOR
     </div>
-    <div>
+    <div className='text-[9.5vw] leading-[10vw]'>
       ALL THINGS
     </div>
-    <div>
+    <div className='text-[9.5vw] leading-[10vw]'>
       CREATIVE
+    </div>
     </div>
     </>
   )
